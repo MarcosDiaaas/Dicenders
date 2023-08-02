021875c41966b30039785bfb564009cd76194b09
Marcos Dias <93786400+MarcosDiaaas@users.noreply.github.com>
2023-08-01 23:58:08 -0300

    Add files via upload

--- config.php
+++ config.php
@@ -3,8 +3,10 @@
     $dbUsername = 'root';
     $dbPassword = '0hHgePORBzg35OYzi3Z3';
     $dbName = 'railway';
+    $dbPort = '5647';
+    $dbSocket = 'mysql://root:0hHgePORBzg35OYzi3Z3@containers-us-west-201.railway.app:5647/railway';
 
-    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName, $dbPort, $dbSocket);
 
     /*
     if($conexao->connect_errno)
